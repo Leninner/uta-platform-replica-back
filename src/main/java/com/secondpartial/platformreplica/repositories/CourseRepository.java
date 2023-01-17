19d92cd8bbb35381f5aa8c17cb7559432a8e366b
leninner <mazabandalenin180@gmail.com>
2023-01-17 17:49:31 -0500

    FIX query

--- src/main/java/com/secondpartial/platformreplica/repositories/CourseRepository.java
+++ src/main/java/com/secondpartial/platformreplica/repositories/CourseRepository.java
@@ -9,7 +9,7 @@ import com.secondpartial.platformreplica.models.CourseModel;
 
 @Repository
 public interface CourseRepository extends JpaRepository<CourseModel, Long> {
-  @Query(value = "select * from course_student cs inner join courses c on c.id = cs.course_id inner join students s on s.id = cs.student_id where s.user_id = :userId", nativeQuery = true)
+  @Query(value = "select c.id, c.description, c.image, c.name, c.semester, c.career_id, c.teacher_id  from course_student cs inner join courses c on c.id = cs.course_id inner join students s on s.id = cs.student_id where s.user_id = :userId", nativeQuery = true)
   public Iterable<CourseModel> findByIdStudent(@Param("userId") Long userId);
 
   @Query(value = "select c.id, c.description, c.image, c.name, c.semester, c.career_id, c.teacher_id from courses c inner join teachers t on t.id = c.teacher_id where t.user_id = :userId", nativeQuery = true)
